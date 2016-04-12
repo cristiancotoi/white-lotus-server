b9aa556655e5126ba3b423c83f2e6db8fe875254
Cristian Cotoi <cristian.cotoi@gmail.com>
2016-04-12 08:47:30 +0300

    Cleaned up code. Working CRUD api.

--- app.js
+++ app.js
@@ -1,78 +1,30 @@
+/**
+ * Created by Sandeep on 01/06/14.
+ */
+
+// Load Our Modules
+
 var express = require('express');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var path = require('path');
-const fs = require('fs');
-
+var persons = require('./routes/persons');
 var mongoose = require('mongoose');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var cors = require('cors');
 
 var app = express();
 
-var models_path = __dirname + '/models';
-console.log(models_path);
-fs.readdirSync(models_path)
-    .forEach(function (file) {
-        if (~file.indexOf('.js')) require(models_path + '/' + file)
-    });
+//connect to our database
+//Ideally you will obtain DB details from a config file
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+var dbName='personDB';
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+var connectionString='mongodb://localhost:27017/'+dbName;
+
+mongoose.connect(connectionString);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
-
-// DB
-mongoose.connect('mongodb://localhost/white-lotus');
-//require('./models/Persons');
-//require('./models/Phases');
-
-// Setup routes
-app.use('/', routes);
-app.use('/users', users);
-
-
+app.use('/api', persons);
 
 module.exports = app;
---
+++ routes/persons.js
@@ -0,0 +1,72 @@
+/**
+ * Created by Sandeep on 01/06/14.
+ */
+
+var Person=require('../models/person');
+var express=require('express');
+
+//configure routes
+
+var router=express.Router();
+
+router.route('/persons')
+    .get(function(req,res){
+       Person.find(function(err,persons){
+           if(err)
+                res.send(err);
+           res.json(persons);
+       });
+    })
+
+    .post(function(req,res){
+        var person=new Person(req.body);
+        person.save(function(err){
+            if(err)
+                res.send(err);
+            res.send({message:'Person Added'});
+        });
+    });
+
+router.route('/persons/:id')
+    .put(function(req,res){
+        Person.findOne({_id:req.params.id},function(err,person){
+
+            if(err)
+                res.send(err);
+
+           for(prop in req.body){
+                person[prop]=req.body[prop];
+           }
+
+            // save the person
+            person.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                res.json({ message: 'Person updated!' });
+            });
+
+        });
+    })
+
+    .get(function(req,res){
+        Person.findOne({_id:req.params.id},function(err, person) {
+            if(err)
+                res.send(err);
+
+            res.json(person);
+        });
+    })
+
+    .delete(function(req,res){
+        Person.remove({
+            _id: req.params.id
+        }, function(err, person) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Successfully deleted' });
+        });
+    });
+
+module.exports=router;
