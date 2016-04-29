1cc39c84885c64c86129d0def70b77b63d793a4f
Cristian Cotoi <cristian.cotoi@gmail.com>
2016-04-30 00:26:53 +0300

    Minor fix for persons update operation

--- routes/persons.js
+++ routes/persons.js
@@ -2,71 +2,72 @@
  * .
  */
 
-var Person=require('../models/person');
-var express=require('express');
+var Person = require('../models/person');
+var express = require('express');
 
 //configure routes
 
-var router=express.Router();
+var router = express.Router();
 
 router.route('/persons')
-    .get(function(req,res){
-       Person.find(function(err,persons){
-           if(err)
+    .get(function (req, res) {
+        Person.find(function (err, persons) {
+            if (err)
                 res.send(err);
-           res.json(persons);
-       });
+            res.json(persons);
+        });
     })
 
-    .post(function(req,res){
-        var person=new Person(req.body);
-        person.save(function(err){
-            if(err)
+    .post(function (req, res) {
+        var person = new Person(req.body);
+        person.save(function (err) {
+            if (err)
                 res.send(err);
-            res.send({message:'Person Added'});
+            res.send({message: 'Person Added'});
         });
     });
 
 router.route('/persons/:id')
-    .put(function(req,res){
-        Person.findOne({_id:req.params.id},function(err,person){
+    .put(function (req, res) {
+        Person.findOne({_id: req.params.id}, function (err, person) {
 
-            if(err)
+            if (err)
                 res.send(err);
 
-           for(prop in req.body){
-                person[prop]=req.body[prop];
-           }
+            let prop;
+            for (prop in req.body) {
+                person[prop] = req.body[prop];
+            }
 
             // save the person
-            person.save(function(err) {
+            person.save(function (err) {
                 if (err)
                     res.send(err);
 
-                res.json({ message: 'Person updated!' });
+                res.json({message: 'Person updated!'});
             });
 
         });
     })
 
-    .get(function(req,res){
-        Person.findOne({_id:req.params.id},function(err, person) {
-            if(err)
+    .get(function (req, res) {
+        Person.findOne({_id: req.params.id}, function (err, person) {
+            if (err)
                 res.send(err);
 
             res.json(person);
         });
     })
 
-    .delete(function(req,res){
+    .delete(function (req, res) {
         Person.remove({
             _id: req.params.id
-        }, function(err, person) {
+        }, function (err, person) {
             if (err)
                 res.send(err);
 
-            res.json({ message: 'Successfully deleted' });
+            res.json({message: 'Successfully deleted'});
         });
     });
 
-module.exports=router;
+module.exports = router;
